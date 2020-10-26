945d00b317cf7d2577f59a8f3b297ed865014163
Gavin Uhran <gavinu2001@gmail.com>
2020-10-26 17:40:52 -0400

    Completed xlsx_to_csv.py Conversion

--- include/init_data.py
+++ include/init_data.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 
 def import_data(file_name, weights):
-    csv_from_excel(file_name)
+    xlsx_to_csv(file_name)
 
     vendors = {}
     with open(os.path.join(DATA_DIR, file_name + '.csv'), 'r', encoding='utf-8-sig') as data:
@@ -15,7 +15,7 @@ def import_data(file_name, weights):
         for row in data_reader:
             # Converts data to proper data types (ints and float for numeric data)
             vendor_name = row[0]
-            order_data = [int(i) for i in row[1:-1]] + [int(row[-1][:-1])]
+            order_data = [int(i) for i in row[1:-1]] + [int(float(row[-1][:-1])*100)]
             
             # Add vendor to dictionary if not already present
             if vendor_name not in vendors:
--- include/xlsx_to_csv.py
+++ include/xlsx_to_csv.py
@@ -3,16 +3,14 @@ import csv
 import sys
 import os
 
+import pandas as pd
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 
-def csv_from_excel(sheet_name):
-    wb = xlrd.open_workbook(os.path.join(DATA_DIR, sheet_name + '.xlsx'))
-    sh = wb.sheet_by_index(0)
-    your_csv_file = open('data/' + sheet_name + '.csv', 'w')
-    wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
-
-    for rownum in range(sh.nrows):
-        wr.writerow(sh.row_values(rownum))
-
-    your_csv_file.close()
+def xlsx_to_csv(sheet_name):
+    # Read and store content of an excel file  
+    read_file = pd.read_excel(os.path.join(DATA_DIR, sheet_name + '.xlsx')) 
+    
+    # Write the dataframe object into csv file 
+    read_file.to_csv('data/' + sheet_name + '.csv', index = None, header = False, float_format = '%.2f%%') 
