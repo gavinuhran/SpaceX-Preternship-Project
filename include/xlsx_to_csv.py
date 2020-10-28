229d2c811eca2de9d3b28fe2710838795d4145df
Tommy Henry <tommyhenry35@gmail.com>
2020-10-28 14:54:22 -0400

    put the app file in include and edited some file path errors

--- include/xlsx_to_csv.py
+++ include/xlsx_to_csv.py
@@ -9,8 +9,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 
 def xlsx_to_csv(sheet_name):
-    # Read and store content of an excel file  
-    read_file = pd.read_excel(os.path.join(DATA_DIR, sheet_name + '.xlsx')) 
-    
-    # Write the dataframe object into csv file 
-    read_file.to_csv('data/' + sheet_name + '.csv', index = None, header = False, float_format = '%.2f%%') 
+    # Read and store content of an excel file
+    read_file = pd.read_excel(os.path.join(DATA_DIR, sheet_name + '.xlsx'))
+
+    # Write the dataframe object into csv file
+    read_file.to_csv('../data/' + sheet_name + '.csv', index = None, header = False, float_format = '%.2f%%') 
