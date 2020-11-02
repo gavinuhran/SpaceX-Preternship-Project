e8f430af71e3a1cf7e7f133eac798d33da929eea
Gavin Uhran <gavinu2001@gmail.com>
2020-11-01 21:42:09 -0500

    Updates to Scoring Formula and File Name Save

--- include/dictionary_functions.py
+++ include/dictionary_functions.py
@@ -1,5 +1,22 @@
 import operator
 
+def get_max_score_from_unsorted(dictionary):
+    vendors = list(dictionary.values())
+    max_score = vendors[0].get_score()
+    for i in range(1, len(vendors)):
+        score = vendors[i].get_score()
+        if score > max_score:
+            max_score = score
+    return max_score
+
+
+def get_max_score_from_descending(descending_dictionary):
+    max = None
+    for key in descending_dictionary:
+        max = descending_dictionary[key].get_score()
+        break
+    return max
+
 def convert_tuples_to_dict(tuples):
     new_dict = {}
 
@@ -16,15 +33,9 @@ def sort_descending(dictionary):
     descending_tuples = sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True)
     return convert_tuples_to_dict(descending_tuples)
 
-def get_max_score(descending_dictionary):
-    max = None
-    for key in descending_dictionary:
-        max = descending_dictionary[key].get_score()
-        break
-    return max
-
 def get_all_scores(dictionary):
+    max_score = get_max_score_from_unsorted(dictionary)
     sorted_dictionary = sort_ascending(dictionary)
-    scores = [dictionary[i].get_score() for i in sorted_dictionary.keys()]
+    scores = [dictionary[i].get_score() / max_score for i in sorted_dictionary.keys()]
 
     return sorted_dictionary.keys(), scores
